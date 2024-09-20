60deb496fa30e7af6acf30e41ce155e31e0187d0
MohitCode17 <mohit.codes17@gmail.com>
2024-09-20 21:20:39 +0530

    add cloudinary sdk setup

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,6 +5,9 @@ const _config = {
   PORT: process.env.PORT,
   FROTNEND_URL: process.env.FROTNEND_URL,
   MONGODB_URL: process.env.MONGODB_URL,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
 
 export const config = Object.freeze(_config);
