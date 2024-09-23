49177eb3d2f8467f110ea30fd4a16f67c43d488f
MohitCode17 <mohit.codes17@gmail.com>
2024-09-23 19:36:05 +0530

    add bid model

---
+++ backend/models/bid.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const bidSchema = new mongoose.Schema({
+  amount: Number,
+  bidder: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    username: String,
+    profileImage: String,
+  },
+  auctionItem: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Auction",
+    required: true,
+  },
+});
+
+const Bid = mongoose.model("Bid", bidSchema);
+
+export default Bid;
